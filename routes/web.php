1d5d3aa2d4f189c9943fff8ca4ca6b793b1f7352
CocoCT19 <lguerrariobo@gmail.com>
2026-03-03 17:57:54 -0500

    Revert "Develop"

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\CollaboratorController;
 
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/dashboard', function () {
-    return response('Dashboard', 200);
-})->middleware('auth');
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/collaborators', [CollaboratorController::class, 'index']);
-    Route::post('/collaborators', [CollaboratorController::class, 'store']);
-    Route::get('/collaborators/{id}/edit', [CollaboratorController::class, 'edit']);
-    Route::put('/collaborators/{id}', [CollaboratorController::class, 'update']);
-    Route::delete('/collaborators/{id}', [CollaboratorController::class, 'destroy']);
-
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
